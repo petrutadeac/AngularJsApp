5c54b6679b4e06532ca7daf57660f315e2eaf267
Petruta Deac <deacpetruta@gmai.com>
2016-12-07 14:25:45 +0200

    First commit

---
+++ sportsstore/resources/orders/get.js
@@ -0,0 +1,7 @@
+
+ if (me === undefined || me.username != "admin") {    cancel("No authorization", 401); }
+ 
+ if (me === undefined || me.username != "admin") {    cancel("No authorization", 401); }
+ 
+ if (me === undefined || me.username != "admin") {    cancel("No authorization", 401); }
+ 
---
+++ sportsstore/resources/products/put.js
@@ -0,0 +1,5 @@
+
+ if (me === undefined || me.username != "admin") {    cancel("No authorization", 401); }
+ 
+ if (me === undefined || me.username != "admin") {    cancel("No authorization", 401); }
+ 
